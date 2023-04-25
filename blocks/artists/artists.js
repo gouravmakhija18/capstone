1f5ed0ccaf46f90731690cd3db097a6c0f04c6ad
Gourav Mukhija <gouravmakhija18@gmail.com>
2023-04-26 00:07:34 +0530

    artists block added

---
+++ blocks/artists/artists.js
@@ -0,0 +1,43 @@
+const pageUrl = `/artists.json`;
+
+async function getArtistsList() {
+  const response = await fetch(`${pageUrl}`);
+  const results = await response.json();
+  const data = results.data;
+
+  return data;
+}
+
+function renderResult(block, result){
+  block.innerHTML="";
+  if (result.length) {
+    const ulElement = document.createElement("ul");
+    result.forEach(({ image_url, name, tags }) => {
+      tags = tags.split(",").join(" | ");
+      const listElement = document.createElement("li");
+      listElement.className = "artist-name";
+      // Set the innerHTML of the div to include the JSON data
+      listElement.innerHTML = `
+        <div class="artist-image">
+          <picture>
+            <source type="image/webp" srcset="${image_url}">
+            <img loading="lazy" alt="${name}" src="${image_url}" />
+          </picture>
+        </div>
+        <div class="artist-info-body">
+          <h2 class="title">${name}</h2>
+          <p>${tags}</p>
+        </div>
+      `;
+
+      // Append the new div element to an existing element in the DOM
+      ulElement.appendChild(listElement);
+    });
+    block.appendChild(ulElement);
+  }
+}
+
+export default async function decorate(block) {
+  const result = await getArtistsList();
+  renderResult(block, result);
+}
